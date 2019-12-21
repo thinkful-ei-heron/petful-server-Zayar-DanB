923f68381daa2979487a6dc090a574ab3aaa635c
zkhin <arandomname@gmail.com>
2019-12-20 23:12:51 -0500

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-dan-zayar.now.sh/',
+  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-dan-zayar.now.sh/',
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
 }
