7e9300b50ef361d4b080e034778549dcf836632d
zkhin <arandomname@gmail.com>
2019-12-20 22:42:06 -0500

    updates

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,7 +1,7 @@
 dogsList = [
   {
     imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      'https://images.dog.ceo/breeds/cotondetulear/IMAG1063.jpg',
     imageDescription:
       'Orange bengal cat with black stripes lounging on concrete.',
     name: 'Fluffy',
@@ -12,7 +12,7 @@ dogsList = [
   },
   {
     imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      'https://images.dog.ceo/breeds/cairn/n02096177_1503.jpg',
     imageDescription:
       'Brown Dog',
     name: 'Chewy',
@@ -23,10 +23,32 @@ dogsList = [
   },
   {
     imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      'https://images.dog.ceo/breeds/corgi-cardigan/n02113186_748.jpg',
     imageDescription:
       'White and brown dog',
     name: 'tim',
+    sex: 'Male',
+    age: 2,
+    breed: 'sheep dog',
+    story: 'found on road',
+  },
+  {
+    imageURL:
+      'https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_2094.jpg',
+    imageDescription:
+      'White and brown dog',
+    name: 'Spot',
+    sex: 'Male',
+    age: 2,
+    breed: 'sheep dog',
+    story: 'found on road',
+  },
+  {
+    imageURL:
+      'https://images.dog.ceo/breeds/komondor/n02105505_2426.jpg',
+    imageDescription:
+      'White and brown dog',
+    name: 'Spot',
     sex: 'Male',
     age: 2,
     breed: 'sheep dog',
@@ -48,7 +70,7 @@ catsList = [
   },
   {
     imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      'http://lorempixel.com/output/cats-q-c-640-480-9.jpg',
     imageDescription:
       'Orange bengal cat with black stripes lounging on concrete.',
     name: 'less fluffy',
@@ -59,14 +81,36 @@ catsList = [
   },
   {
     imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      'http://lorempixel.com/output/cats-q-c-640-480-1.jpg',
     imageDescription:
-      'Orange bengal cat with black stripes lounging on concrete.',
+      'Kitty.',
     name: 'Greedo',
     sex: 'non-binary',
     age: 1,
     breed: 'Rodian',
     story: 'More babies',
+  },
+  {
+    imageURL:
+      'http://lorempixel.com/output/cats-q-c-640-480-7.jpg',
+    imageDescription:
+      'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Greedo',
+    sex: 'unknown',
+    age: 1,
+    breed: 'Rodian',
+    story: 'More babies',
+  },
+  {
+    imageURL:
+      'http://lorempixel.com/output/cats-q-c-640-480-8.jpg',
+    imageDescription:
+      'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Greedo',
+    sex: 'unknown',
+    age: 1,
+    breed: 'Rodian',
+    story: 'Plotted owner\'s demise',
   },
 ];
 
